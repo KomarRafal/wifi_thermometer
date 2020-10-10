1242dbc47967d39754008cd351b658a74b9bf74f
Rafal <komarro@gmail.com>
2020-10-10 12:05:05 +0200

    DeepSleep

--- main.lua
+++ main.lua
@@ -1,10 +1,33 @@
+--[[
+TODO:
+- static IP
+- web base wifi configurtion
+- parameterize temperature channel
+]]--
+
 local SSID = "***REMOVED***"
 local PASSWORD = "***REMOVED***"
 local OW_PIN = 3
 
+local INSIDE = 1
+local OUTSIDE = 2
+
+if not PRODUCTION then
+  PRODUCTION = 1
+end
+local SLEEP_TIME = 600000000
+if PRODUCTION == 0 then
+  SLEEP_TIME = 5000000
+end
+  
 local function panic(...)
   print("ERROR!".. ... .."Rebooting...\n\n")
   node.restart()
+end
+
+local function do_sleep()
+  print(string.format("DeepSleep for %d [us]",SLEEP_TIME))
+  node.dsleep(SLEEP_TIME, 3)
 end
 
 local function send_temp(temperature)
@@ -19,7 +42,7 @@ local function send_temp(temperature)
     )
     -- api.thingspeak.com 184.106.153.149
     conn:connect(80, "184.106.153.149")
-    conn:send("GET /update?key=***REMOVED***&field2=" .. temperature .. " HTTP/1.1\r\n")
+    conn:send("GET /update?key=***REMOVED***&field" ..OUTSIDE.. "=" .. temperature .. " HTTP/1.1\r\n")
     conn:send("Host: api.thingspeak.com\r\n")
     conn:send("Accept: */*\r\n")
     conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
@@ -29,12 +52,14 @@ local function send_temp(temperature)
         function(conn)
             print("Closing connection")
             conn:close()
+            do_sleep()
         end
     )
     conn:on(
         "disconnection",
         function(conn)
             print("Got disconnection...")
+            do_sleep()
         end
     )
 end
@@ -42,28 +67,23 @@ end
 local function handle_temp(temp)
   if #ds18b20.sens == 0 then
     print("Sensor not found.")
+    do_sleep()
     return
-  end
+  end  
   for addr, temp in pairs(temp) do
     temperature = temp
   end
   print(string.format("Temp: %s C", temperature))
   if PRODUCTION == 1 then
     send_temp(temperature)
+  else
+    do_sleep()
   end
 end
 
 local function meassure_temperatur()
   ds18b20 = require("ds18b20")
-  
-  local temp_timer = tmr.create()
-  if not temp_timer:alarm(TEMP_TIME, tmr.ALARM_AUTO, 
-    function()
-      ds18b20:read_temp(handle_temp, pin, ds18b20.C, nil)
-    end)
-  then
-    panic("ERROR")
-  end
+  ds18b20:read_temp(handle_temp, pin, ds18b20.C, nil)
 end
 
 local function got_ip(info)
@@ -87,14 +107,6 @@ local function init_wifi()
 end
 
 local function start()
-  if not PRODUCTION then
-      PRODUCTION = 0 -- CHANGE IT!
-  end
-  if PRODUCTION == 1 then
-    TEMP_TIME = 120000
-  else
-    TEMP_TIME = 1000
-  end
   init_wifi()
 end
 
