48be26a58a9d7546829c8b12b5ce8013128c3f77
Rafal <komarro@gmail.com>
2020-10-13 21:53:26 +0200

    ThingSpeak API key and field added

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 --[[
 TODO:
 - static IP, maybe in CONFIG_FILE
-- web base wifi configurtion
-- parameterize temperature field (outside, inside) (maybe from webpage?)
 - blink led when connecting
+- turn off power led while sleeping
+- web page:
+- * read frequency (10min, 15min, 30min)
 ]]--
 
 if not BUTTON then
@@ -14,14 +15,10 @@ local CONFIG_FILE = "eus_params.lua"
 local WIFI_TIMEOUT = 20000
 local OW_PIN = 3
 
-local INSIDE = 1
-local OUTSIDE = 2
-
-local KEY_CHOPINA = "***REMOVED***"
-local KEY_BRENNA = "***REMOVED***"
+local config = nil
 
 -- Uncomment it if you want to just see tempreature without sending
-PRODUCTION = 0
+--PRODUCTION = 0
 
 if not PRODUCTION then
   PRODUCTION = 1
@@ -48,7 +45,7 @@ local function send_temp(temperature)
     )
     -- api.thingspeak.com 184.106.153.149
     conn:connect(80, "184.106.153.149")
-    conn:send("GET /update?key=" .. KEY_BRENNA .. "&field" .. INSIDE .. "=" .. temperature .. " HTTP/1.1\r\n")
+    conn:send("GET /update?key=" .. config.write_key .. "&field" .. config.field_nbr .. "=" .. temperature .. " HTTP/1.1\r\n")
     conn:send("Host: api.thingspeak.com\r\n")
     conn:send("Accept: */*\r\n")
     conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
@@ -114,10 +111,17 @@ end
 
 local function configure_wifi()
   print("Configuring wifi...")
+--  Watchdog
+    tmr.create():alarm(300000, tmr.ALARM_SINGLE, function()
+      node.restart()
+    end)
+--  wifi.setmode(wifi.STATIONAP)
+--  wifi.ap.config({ssid="TermometrWiFi", auth=wifi.OPEN})
+--  enduser_setup.manual(true)
   enduser_setup.start(
     function()
+      print("Connected to wifi as: " .. wifi.sta.getip())
       tmr.create():alarm(3000, tmr.ALARM_SINGLE, function()
-        print("Connected to wifi as: " .. wifi.sta.getip())
         node.restart()
       end)
     end,
@@ -143,9 +147,9 @@ local function start()
   elseif not file.exists(CONFIG_FILE) then
     configure_wifi()
   else
-    p = dofile(CONFIG_FILE)
+    config = dofile(CONFIG_FILE)
     my_wifi = require("my_wifi")
-    my_wifi:init_wifi(p.wifi_ssid, p.wifi_password, got_ip, 20000, wifi_timeout)
+    my_wifi:init_wifi(config.wifi_ssid, config.wifi_password, got_ip, 20000, wifi_timeout)
   end
 end
 
